89d4087b4a73c220caa12093f4e1e8394a738028
Álisson Véras <59287050+fty57@users.noreply.github.com>
2022-06-04 10:00:02 -0300

    Correção do exercício aula 8

--- src/aula8/index.js
+++ src/aula8/index.js
@@ -1,8 +1,18 @@
-const nome = 'Álisson Véras';
-const sobrenome = 'Damasceno'
-const idade = '21';
-const peso = '65';
-const alturaEmM = 1.81;
+/**
+ * Álisson Véras Damasceno tem 21 anos, pesa 65kg tem 1.81 de altura e seu IMC é de 19.84
+ * Álisson Véras nasceu em 2001
+ */
 
-console.log(`Nome: ${nome} ${sobrenome} Idade: ${idade} Peso: ${peso} Altura: ${alturaEmM}`);
-console.log(typeof alturaEmM);
+const nome = "Álisson Véras";
+const sobrenome = "Damasceno";
+const idade = "21";
+const anoNascimento = "2001";
+const altura = 1.81;
+const peso = 65;
+let imc;
+
+imc = peso / (altura * altura);
+
+console.log(`${nome} ${sobrenome} tem ${idade} anos, pesa ${peso}kg tem ${altura} de altura e seu IMC é de ${imc.toFixed(2)}`);
+
+console.log(`${nome} nasceu em ${anoNascimento}`);
