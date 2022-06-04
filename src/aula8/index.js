044ea19dcb415dcbdeca0fd3570693fc5192a3ba
Álisson Véras <59287050+fty57@users.noreply.github.com>
2022-06-04 09:50:21 -0300

    Exercício aula 08

---
+++ src/aula8/index.js
@@ -0,0 +1,8 @@
+const nome = 'Álisson Véras';
+const sobrenome = 'Damasceno'
+const idade = '21';
+const peso = '65';
+const alturaEmM = 1.81;
+
+console.log(`Nome: ${nome} ${sobrenome} Idade: ${idade} Peso: ${peso} Altura: ${alturaEmM}`);
+console.log(typeof alturaEmM);
