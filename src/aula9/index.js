d5773c61fc20e1fb49f827fcf8b4b9630983df33
Álisson Véras <59287050+fty57@users.noreply.github.com>
2022-06-05 09:45:40 -0300

    Let vs Var

---
+++ src/aula9/index.js
@@ -0,0 +1,13 @@
+// O var permite redeclarar a variável
+var nome = 'Luiz';
+var nome = 'Otávio';
+console.log(nome);
+
+// O let não permite redeclarar a variável
+let name = 'Álisson';
+let name = 'Véras';
+console.log(name);
+
+// Não faça dessa maneira, a variável fica global
+nickname = 'Ally'
+console.log(nickname);
