ec823f1694bb2c210dd8bf2ed640b2163111370e
Álisson Véras <59287050+fty57@users.noreply.github.com>
2022-06-06 14:18:33 -0300

    Operadores aritméticos

---
+++ src/aula10/index.js
@@ -0,0 +1,24 @@
+/**
+ * Aritméticos
+ * + Adição / Concatenação
+ * - Subtração
+ * / Divisão
+ * * Multiplicação
+ * ** Potenciação
+ * % Resto da divisão
+ * () ** * / % + - Ordem de precedência
+ * ++ Incremento
+ * -- Decremento
+ * += -= *= /= %= **= ++ -- Operadores de atribuição
+ * parseInt(), parseFloat(), Number()
+ */
+const num1 = 5;
+const num2 = 2;
+const num3 = 10;
+const num4 = parseFloat('4.21');
+const num5 = Number('2.21');
+let contador = 2;
+contador **= 10;
+console.log(num5 * num1);
+console.log(typeof num5);
+
