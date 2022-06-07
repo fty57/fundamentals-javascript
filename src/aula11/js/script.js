7ebc7f66b09969329e62e8a59b1f17523c9976d1
Álisson Véras <59287050+fty57@users.noreply.github.com>
2022-06-07 07:44:18 -0300

    15. Exercícios - Solução

--- src/aula11/js/script.js
+++ src/aula11/js/script.js
@@ -4,6 +4,6 @@ let num2 = prompt('Digite outro número: ');
 
 num1 = Number(num1);
 num2 = Number(num2);
-let resultado = num1 + num2;
 
-alert('O resultado foi: ' + resultado);
+// Pode colocar uma expressão aqui dentro
+alert(`O resultado da sua conta foi: ${num1 + num2}`);
