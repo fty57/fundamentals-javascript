8b1dd554c8f9e678fa75515b6f84f8c4fccad749
Álisson Véras <59287050+fty57@users.noreply.github.com>
2022-06-08 08:21:50 -0300

    17. Mais sobre strings

---
+++ src/aula13/index.js
@@ -0,0 +1,18 @@
+// A "\" escapa o próximo caractere
+// Se você deseja colocar uma "\" utilize duas "\\"
+// CharAt(posição) - Retorna o caractere na posição especificada
+// CharCodeAt(posição) - Retorna o código do caractere na posição especificada
+// Concat(string) - Concatena a string passada como parâmetro com a string atual
+// IndexOf(string) - Retorna a posição da primeira ocorrência da string passada como parâmetro
+// LastIndexOf(string) - Retorna a posição da última ocorrência da string passada como parâmetro
+// Search(string) - Retorna a posição da primeira ocorrência da string passada como parâmetro
+// Replace(string, string) - Substitui todas as ocorrências da string passada como parâmetro pela string passada como segundo parâmetro
+// Slice(início, fim) - Retorna uma string com a parte da string atual entre os valores de início e fim
+// Substring(início, fim) - Retorna uma string com a parte da string atual entre os valores de início e fim
+// Split(separador) - Retorna um array com a string atual separada por cada caractere do separador
+// ToLowerCase() - Converte toda a string atual para minúsculas
+// ToUpperCase() - Converte toda a string atual para maiúsculas
+let umaString = "O rato roeu a roupa do rei de roma.";
+
+
+console.log(umaString.toUpperCase());
