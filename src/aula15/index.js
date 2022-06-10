4b393a11e52aa3ea0e9566881ae8ed042a3ee95e
Álisson Véras <59287050+fty57@users.noreply.github.com>
2022-06-10 10:40:59 -0300

    20. Objeto Math

---
+++ src/aula15/index.js
@@ -0,0 +1,19 @@
+// Math.floor(): arredonda para baixo
+// Math.ceil(): arredonda para cima
+// Math.round(): arredonda para cima se for par, para baixo se for impar
+// Uma função que está dentro de um objeto, chama-se de método
+// Math.max(1, 2, 3, 4, 5): retorna o maior número
+// Math.min(1, 2, 3, 4, 5): retorna o menor número
+// Math.random(): retorna um número aleatório entre 0 e 1
+// (10 - 5) + 5: é para ficar entre 10 e 5 no random()
+// Infinity: retorna um número infinito
+
+console.log(100 / 0);
+
+// console.log(Math.sqrt(num1));
+// let num2 = Math.floor(num1);
+// let num2 = Math.ceil(num1);
+// let num2 = Math.round(num1);
+// console.log(Math.max(1,2,3,4,5,6,-10,1500, 9 , 8 ))
+// console.log(Math.min(1,2,3,4,5,6,-10,1500, 9 , 8 ))
+// const aleatorio = Math.round(Math.random() * (10 - 5) + 5);
