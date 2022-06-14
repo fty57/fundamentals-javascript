62d8543ed0df7bc4f30002a2b941a23da9e3d66e
Álisson Véras <59287050+fty57@users.noreply.github.com>
2022-06-14 08:06:49 -0300

    22. Arrays (Básico)

---
+++ src/aula16/index.js
@@ -0,0 +1,43 @@
+//            01234567890123456
+const nome = 'Francisco Álisson'
+
+//                0        1         2         
+const alunos = ['Luiz', 'Maria', 'João'];
+
+// console.log(typeof alunos); // object
+// console.log(alunos instanceof Array); 
+
+// slice(inicio, fim) - retorna um novo array
+// console.log(alunos.slice(0, 3));
+// console.log(alunos.slice(0, -2));
+
+// console.log(alunos[50]);
+
+// Deletar aluno da lista
+//delete alunos[1];
+
+// const removido = alunos.pop(); // remove o último elemento
+// const removidoV2 = alunos.shift(); // remove o primeiro elemento
+// console.log(removido);
+// console.log(removidoV2);
+
+// Adiciona no começo do array
+// alunos.unshift('Francisco');
+// alunos.unshift('Pedro');
+
+// Adiciona no fim do array
+// alunos.push('Pedro');
+// alunos.push('Fábio');
+// alunos.push('Luana');
+// alunos[alunos.length] = 'Pedro';
+// alunos[alunos.length] = 'Fábio';
+// alunos[alunos.length] = 'Luana';
+
+// Altera o valor do índice
+// alunos[0] = 'Eduardo';
+// alunos[3] = 'Pedro';
+
+
+// console.log(alunos[0]);
+// console.log(alunos[1]);
+// console.log(alunos[2]);
