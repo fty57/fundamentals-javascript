2cae062552d2afa4832ee8eadfd72e10d7a84323
Álisson Véras <59287050+fty57@users.noreply.github.com>
2022-06-14 08:30:11 -0300

    24. Funções (Básico)

---
+++ src/aula17/index.js
@@ -0,0 +1,31 @@
+/*
+function soma(x = 1,y = 20){
+     const resultado = x + y;
+     return resultado;
+}
+
+const resultado = soma(4,2);
+console.log(resultado);
+*/
+
+/*
+function saudacao(nome) {
+     console.log(`Bom dia ${nome}!`);
+     return `Bom dia ${nome}!`;
+};
+
+const variavel = saudacao('Álisson');
+console.log(variavel);
+*/
+
+/*
+const raiz = function(n){
+     return Math.sqrt(n);
+}
+*/
+
+const raiz = (n) => Math.sqrt(n);
+
+console.log(raiz(9));
+console.log(raiz(16));
+console.log(raiz(25));
