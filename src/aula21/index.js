65a2e86ea1e59a9af18ea8126ed4ab5d8638c794
Álisson Véras <59287050+fty57@users.noreply.github.com>
2022-06-17 10:57:21 -0300

    2. Operadores de comparação

---
+++ src/aula21/index.js
@@ -0,0 +1,15 @@
+/*
+Operadores de comparação
+>   Maior que
+<   Menor que
+>=  Maior ou igual
+<=  Menor ou igual
+==  Igual a  (valor)
+!=  Diferente de (valor)
+=== Identico a (valor e tipo) // Preferência de tipo
+!== Não identico a (valor e tipo) // Preferência de tipo
+*/  
+const num1 = 10;
+const num2 = '10';
+const comp = num1 !== num2;
+console.log(comp);
