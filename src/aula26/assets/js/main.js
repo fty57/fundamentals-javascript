30731eacc16ff946213d66f0ee4989a6b9f4f28e
Álisson Véras <59287050+fty57@users.noreply.github.com>
2022-06-21 09:36:36 -0300

    9.2 Exercício

--- src/aula26/assets/js/main.js
+++ src/aula26/assets/js/main.js
@@ -19,8 +19,34 @@ form.addEventListener("submit", (e) => {
     return;
   }
 
-  console.log("Cheguei aqui...");
+  const imc = getImc(peso, altura);
+  const nivelImc = getNivelImc(imc);
+  const msg = `Seu IMC é ${imc} e você está ${nivelImc}`;
+  setResultado(msg, true);
 });
+
+function getNivelImc(imc) {
+  const nivel = [
+    "Abaixo do peso",
+    "Peso normal",
+    "Sobrepeso",
+    "Obesidade grau 1",
+    "Obesidade grau 2",
+    "Obesidade grau 3",
+  ];
+
+  if (imc >= 39.9) return nivel[5];
+  if (imc >= 34.9) return nivel[4];
+  if (imc >= 29.9) return nivel[3];
+  if (imc >= 24.9) return nivel[2];
+  if (imc >= 18.5) return nivel[1];
+  if (imc < 18.5) return nivel[0];
+}
+
+function getImc(peso, altura) {
+  const imc = peso / (altura * altura);
+  return imc.toFixed(2);
+}
 
 function criaP() {
   const p = document.createElement("p");
@@ -30,7 +56,14 @@ function criaP() {
 function setResultado(text, isValid) {
   const resultado = document.querySelector("#resultado");
   resultado.innerHTML = ""; // Limpando o HTML
+
   const p = criaP();
+
+  if (isValid) {
+    p.classList.add("paragrafo-sucesso");
+  } else {
+    p.classList.add("paragrafo-erro");
+  }
   p.innerHTML = text;
   resultado.appendChild(p);
 }
