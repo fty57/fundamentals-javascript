d22fd616b6ebc8b9957c30d35bdf36631f8e00f0
Álisson Véras <59287050+fty57@users.noreply.github.com>
2022-06-27 16:21:17 -0300

    17. For - Clássico - Estrutura de repetição

--- src/aula34/index.js
+++ src/aula34/index.js
@@ -9,5 +9,23 @@ console.log("Linha 5");
 
 // i - index
 for (let i = 0; i <= 5; i++) {
-  console.log(i);
+  console.log(`Linha ${i}`);
+}
+
+for (let i = 400; i <= 500; i += 10) {
+  console.log(`Linha ${i}`);
+}
+
+for (let i = 500; i <= 400; i -= 10) {
+  console.log(`Linha ${i}`);
+}
+
+for (let i = 0; i <= 10; i++) {
+  const par = i % 2 === 0 ? "par" : "impar";
+  console.log(i, par);
+}
+
+const frutas = ["maça", "pera", "uva"];
+for (let i = 0; i < frutas.length; i++) {
+  console.log(`Frutas ${frutas[i]}`);
 }
