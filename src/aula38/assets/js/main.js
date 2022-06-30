d56f4d66d362c9e0286704ae7b4f943f12e2f81f
Álisson Véras <59287050+fty57@users.noreply.github.com>
2022-06-30 19:34:47 -0300

    22. Exercícios com NodeList

--- src/aula38/assets/js/main.js
+++ src/aula38/assets/js/main.js
@@ -1,1 +1,12 @@
 const divParagrafos = document.querySelector(".paragrafos");
+const ps = divParagrafos.querySelectorAll("p");
+
+const estilosBody = getComputedStyle(document.body);
+const backgroundColor = estilosBody.backgroundColor;
+console.log(backgroundColor);
+
+for (let p of ps) {
+  console.log(p);
+  p.style.backgroundColor = backgroundColor;
+  p.style.color = "white";
+}
