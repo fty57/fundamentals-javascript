516a48be6c9dce8e1ba1b76f3dfdbffa34075d49
Álisson Véras <59287050+fty57@users.noreply.github.com>
2022-08-13 11:49:57 -0300

    5. Closures

---
+++ src/aula53/index.js
@@ -0,0 +1,15 @@
+// Global
+// Closure é a habilidade da função em acessar o seu escopo léxico
+function retornaFuncao(nome) {
+  // const nome = "Luiz";
+  return function () {
+    return nome;
+  };
+}
+
+const funcao = retornaFuncao("Luiz");
+const funcao2 = retornaFuncao("João");
+console.dir(funcao);
+console.dir(funcao2);
+
+console.log(funcao(), funcao2());
