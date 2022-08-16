80a643df3af5a85ee5601fdb37060251131d7d85
Álisson Véras <59287050+fty57@users.noreply.github.com>
2022-08-16 08:46:15 -0300

    11. Calculadora com função construtora

--- src/aula58/index.js
+++ src/aula58/index.js
@@ -1,7 +1,11 @@
 // Função fábrica -> objetos criaPessoa
 // Função construtora -> objetos Pessoa (new)
 function Pessoa(nome, sobrenome) {
+  // Atributos ou métodos privados
   const ID = 1234;
+  const metodoInterno = function () {};
+
+  // Atributos ou métodos públicos
   this.nome = nome;
   this.sobrenome = sobrenome;
 
